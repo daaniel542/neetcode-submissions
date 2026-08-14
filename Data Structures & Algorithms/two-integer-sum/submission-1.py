528b33bcfc22d67f0e6a93491d2addf606c4b7a3
Daniel Yang <ydsyang@ucdavis.edu>
2026-08-14 10:52:31 +0300

    Update: two-integer-sum - submission-1

--- Data Structures & Algorithms/two-integer-sum/submission-1.py
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -1,16 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Initialize our hashmap to store {number: index}
-        hashmap = {}
+        output = {}
+
+        for index, value in enumerate(nums):
+            difference = target - value
+            if difference in output:
+                return [output[difference], index] 
+            output[value] = index
+            
         
-        # enumerate gives us both the index (i) and the value (num)
-        for i, num in enumerate(nums):
-            difference = target - num
+       
             
-            # Check if the difference is already a key in our hashmap
-            if difference in hashmap:
-                # If it is, return the index of the difference, and our current index
-                return [hashmap[difference], i]
-            
-            # If not, add the current number and its index to the hashmap for future checks
-            hashmap[num] = i
