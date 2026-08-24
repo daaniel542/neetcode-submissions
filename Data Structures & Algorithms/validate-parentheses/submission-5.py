ee906643c52f56a04b39bedd8497c86321e09f24
Daniel Yang <ydsyang@ucdavis.edu>
2026-08-24 21:38:02 +0300

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        # Edit 1: Added a dictionary to match brackets
+        mapping = {")": "(", "}": "{", "]": "["}
+
+        for value in s:
+            if value in "({[":
+                stack.append(value)
+            
+            elif value in ')}]':
+                # Edit 2: Check if stack is empty, and compare using the dictionary
+                if not stack or stack.pop() != mapping[value]:
+                    return False
+
+        # alternate way of writing this using the 'mapping' map
+            #for value in s:
+                #if value in mapping.values():
+                    #stack.append(value)
+            
+            # elif value in mapping:
+                #if not stack or stack.pop() != mapping[value]:
+                   # return False
+
+        # Edit 3: Return True only if the stack is completely empty at the end
+        return len(stack) == 0
